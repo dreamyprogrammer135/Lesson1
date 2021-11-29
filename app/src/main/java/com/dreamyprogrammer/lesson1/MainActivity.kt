e5a3aa7eb037d4c17b040f664fcfdbac8a8c1e93
Litvinov Ilya <dreameprogrammer82@gmail.com>
2021-11-29 19:51:49 +0300

    corrected the error

--- app/src/main/java/com/dreamyprogrammer/lesson1/MainActivity.kt
+++ app/src/main/java/com/dreamyprogrammer/lesson1/MainActivity.kt
@@ -16,22 +16,27 @@ class MainActivity : AppCompatActivity(), MainView {
         vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb?.root)
 
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
+        vb?.btnCounter1?.setOnClickListener{
+            presenter.counterClick1()
         }
-
-        vb?.btnCounter1?.setOnClickListener(listener)
-        vb?.btnCounter2?.setOnClickListener(listener)
-        vb?.btnCounter3?.setOnClickListener(listener)
+        vb?.btnCounter2?.setOnClickListener {
+            presenter.counterClick2()
+        }
+            vb?.btnCounter3?.setOnClickListener{
+                presenter.counterClick3()
+        }
     }
 
-    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
-    override fun setButtonText(index: Int, text: String) {
-        when(index){
-            0 -> vb?.btnCounter1?.text = text
-            1 -> vb?.btnCounter2?.text = text
-            2 -> vb?.btnCounter3?.text = text
-        }
+    override fun setButtonText1(text: String) {
+        vb?.btnCounter1?.text = text
+    }
+
+    override fun setButtonText2(text: String) {
+        vb?.btnCounter2?.text = text
+    }
+
+    override fun setButtonText3(text: String) {
+        vb?.btnCounter3?.text = text
     }
 
 }
