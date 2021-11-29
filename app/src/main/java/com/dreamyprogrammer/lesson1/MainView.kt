2de8b422dc5e15f3c414467f9db4e4d9ade4370c
Litvinov Ilya <dreameprogrammer82@gmail.com>
2021-11-29 19:30:59 +0300

    Подготовка к Домашке N1

---
+++ app/src/main/java/com/dreamyprogrammer/lesson1/MainView.kt
@@ -0,0 +1,5 @@
+package com.dreamyprogrammer.lesson1
+
+interface MainView {
+    fun setButtonText(index: Int, text: String)
+}
---
+++ app/src/main/java/com/dreamyprogrammer/lesson1/Model.kt
@@ -0,0 +1,9 @@
+package com.dreamyprogrammer.lesson1
+
+class Model {
+    fun getGreeting(): String {
+      //  ...
+        return "Hello!"
+    }
+
+}
